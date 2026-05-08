cab9cc5068464e746c3fbd27d88f22f8eef5e539
Antonio <antoniomcfms@gmail.com>
2026-05-08 15:22:29 +0100

    Funções adicionadas

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void iniciarImagem(char imagem[][], int largura, int altura){
+
+}
+
+void desenharPontos(char imagem[][], int x, int y){
+
+}
+
+void desenharLinha(char imagem[][], int x1, int y1, int x2, int y2){
+
+}
+
+void desenharRetangulo(char imagem[][], int x1, int y1, int x2, int y2){
+
+}
+
+void guardarImagem(char imagem[][], int largura, int altura, const char* nomeFicheiro){
+
+}
