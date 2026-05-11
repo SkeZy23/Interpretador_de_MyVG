4f4a971188de8c42a582aaa191e88ef9a87cba8a
Antonio <antoniomcfms@gmail.com>
2026-05-11 10:28:42 +0100

    função main feita

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void desenharPontos(struct Imagens* img, int x, int y){
     /*António*/
     img->imagem;
     if(x >= 0 && x < img->largura && y >= 0 && y < img->altura) {
-        img->imagem[y][x] = '*';
+        img->imagem[y][x] = '#';
     }
 }
 
@@ -40,4 +40,28 @@ void guardarImagem(struct Imagens* img, int largura, int altura, const char* nom
 
 int main(){
     /*António*/
+    struct Imagens img;
+    printf("Qual o nome do ficheiro em MyVG? ");
+    char nomeFicheiro[100];
+    scanf("%s", nomeFicheiro);
+    // quero ler o ficheiro e agarrar nos primeiros 2 numeros para a largura e altura da imagem
+    int largura, altura;
+    fscanf(fopen(nomeFicheiro, "r"), "%d %d", &largura, &altura);
+    iniciarImagem(&img, largura, altura);
+    // quero ler o resto do ficheiro e desenhar os pontos, linhas e retângulos
+    char tipoDesenho[20];
+    while(fscanf(fopen(nomeFicheiro, "r"), "%s", tipoDesenho) != EOF) {
+        if(strcmp(tipoDesenho, "p") == 0) {
+            int x, y;
+            fscanf(fopen(nomeFicheiro, "r"), "%d %d", &x, &y);
+            desenharPontos(&img, x, y);
+        } else if(strcmp(tipoDesenho, "l") == 0) {
+            int x1, y1, x2, y2;
+            fscanf(fopen(nomeFicheiro, "r"), "%d %d %d %d", &x1, &y1, &x2, &y2);
+            desenharLinha(&img, x1, y1, x2, y2);
+        } else if(strcmp(tipoDesenho, "r") == 0) {
+            int x1, y1, x2, y2;
+            fscanf(fopen(nomeFicheiro, "r"), "%d %d %d %d", &x1, &y1, &x2, &y2);
+            desenharRetangulo(&img, x1, y1, x2, y2);
+        }
 }
